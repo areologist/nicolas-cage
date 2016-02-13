1315654ab9401ae13fb2debe0f9444dc8e3047b8
Shaun O'Brien <sobriendev@gmail.com>
2016-02-13 01:44:17 -0500

    🍃  init fake tests 🍂

--- client/app/components/gallery/gallery.spec.js
+++ client/app/components/gallery/gallery.spec.js
@@ -1,1 +1,43 @@
-// TODO: this
+import {gallery} from './gallery';
+import {galleryComponent} from './gallery.component';
+import {GalleryController} from './gallery.controller';
+
+// contains lame tests, but you get the idea
+
+describe('Gallery', () => {
+  let $rootScope;
+  let makeController;
+
+  beforeEach(window.module(gallery.name));
+  beforeEach(inject(_$rootScope_ => {
+    $rootScope = _$rootScope_;
+    makeController = (injectables) => {
+      return new GalleryController(injectables);
+    };
+  }))
+
+  describe('module', () => {
+    it('should have an appropriate name', () => {
+      expect(gallery.name).toEqual('gallery');
+    });
+  });
+
+  describe('component', () => {
+    let sut;
+    beforeEach(() => {
+      sut = galleryComponent();
+    });
+
+    it('should have the right controller', () => {
+      expect(sut.controller).toEqual(GalleryController);
+    });
+  });
+
+  describe('controller', () => {
+    it('should have gallery items', () => {
+      const controller = makeController();
+      expect(typeof controller.stickers).toBe('object');
+      expect(controller.stickers.length).toBeDefined();
+    });
+  });
+});

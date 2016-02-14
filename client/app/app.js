3fdb54672bb477afe2a401b1fa63a01c114dc6f2
Shaun O'Brien <sobriendev@gmail.com>
2016-02-14 03:23:55 -0500

    🌙  late 🌘

--- client/app/app.js
+++ client/app/app.js
@@ -8,16 +8,12 @@ import uiRouter from 'angular-ui-router';
 import ngAnimate from 'angular-animate';
 
 import {shared} from './shared/shared';
-import {home} from './components/home/home';
-import {gallery} from './components/gallery/gallery';
-import {about} from './components/about/about';
+import {components} from './components/components';
 
 angular.module('app', [
   uiRouter,
   ngAnimate,
   shared.name,
-  home.name,
-  gallery.name,
-  about.name
+  components.name
 ])
 .component('app', appComponent);
--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,17 @@ module.exports = {
       { test: /\.html$/, loader: 'raw' },
       { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.css/, loader: 'style!css' },
+      { test: /\.png$/, loader: 'url-loader?limit=44032' },
       {
-        test: /\.js$/,
-        loader: 'babel',
+        test: /\.js$/, loader: 'babel',
         exclude: [/client\/lib/, /node_modules/, /\.spec\.js/]
       },
       // loaders for font-awesome import
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&minetype=application/font-woff"
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
       },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
-      }
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
     ]
   }
 };
